dc3ec433034c8e97b742a83c093ee6b11586fe12
NataliIvantsova <ivancovanm@yandex.ru>
2023-01-19 22:04:15 +0300

    Add Exercise202

---
+++ Exercise202/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int InputInt(string messege)
+{
+    System.Console.Write(messege);//введите сообщение
+    int x = Convert.ToInt32(Console.ReadLine());//значению х = прочитать строку
+    return x;//вывести значение х из предыдущей строки
+}
+
+int a = InputInt("Введите число > ");
+
+if(a<100)
+{
+    System.Console.Write($"в числе {a} третьей цифры нет");
+}
+else
+{
+     while(a>=1000)
+     {
+        a = a/10;
+     }
+     int result = a%10;
+     System.Console.WriteLine($"{result}");
+}
